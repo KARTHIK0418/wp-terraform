a4853dd32e51a330c03fc1293c549de45c73480c
KARTHIK0418 <147519514+KARTHIK0418@users.noreply.github.com>
2024-03-20 17:28:09 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 sudo yum update -y
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo chmod 666 /var/run/docker.sock
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+cd
 sudo yum install git -y
-git clone https://github.com/KARTHIK0418/car-prediction.git
-cd car-prediction
-pip3 install -r requirements.txt
-screen -m -d python3 app.py
+git clone https://github.com/KARTHIK0418/docker-compose_wp.git
+cd docker-compose_wp
+docker-compose up -d
