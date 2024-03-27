a14550609d16d1735649bb2f1a453a29eb38d24f
KARTHIK0418 <147519514+KARTHIK0418@users.noreply.github.com>
2024-03-27 16:07:47 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "wordpress" {
   ami                         = "ami-0cf7b2f456cd5efd4"
   instance_type               = "t2.micro"
-  key_name                    = "ireland"
+  key_name                    = "new-key"
   vpc_security_group_ids      = [aws_security_group.demosg.id]
   subnet_id                   = aws_subnet.subnet1.id
   associate_public_ip_address = true
